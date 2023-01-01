5772b4a4adaf715bc3ba18b943b546d8ee890e9d
Ahmed Esmat <ahmedesmat01@yahoo.co.uk>
2023-01-01 16:53:43 +0200

    added path

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express();
 const PORT = 3000;
 // app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: [
+      "https://peaceful-biscotti-be0eb6.netlify.app",
+      "https://peaceful-biscotti-be0eb6.netlify.app/login",
+    ],
+  })
+);
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(router);
