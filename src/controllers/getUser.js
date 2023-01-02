33bfcc83726b3a69a613e666d289864829411d00
Ahmed Esmat <ahmedesmat01@yahoo.co.uk>
2023-01-02 23:40:15 +0200

    modified getUser

--- src/controllers/getUser.js
+++ src/controllers/getUser.js
@@ -5,15 +5,15 @@ const getUser = async (req, res) => {
     const user = await User.findOne({ email }).exec();
   
     if (!user) {
-      return res.status(400).json({ message: "Invalid email or password." });
+      return res.status(400).json({ status: "Invalid email or password." });
     }
   
     // Check if the provided password matches the hashed password in the database
     if (password !== user.password) {
-      return res.status(400).json({ message: "Invalid email or password." });
+      return res.status(400).json({ status: "Invalid email or password." });
     }
   
-    return res.status(200).json({ message: "Successfully logged in." });
+    return res.status(200).json({ status: "Successfully logged in." });
   }
 
 module.exports = getUser;
